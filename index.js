12e6b83006d03b76d02dff6900c4203133bd7eb9
TATSUKI ADANIYA <tatsukiadaniya@tatsuki.local>
2016-10-04 21:37:30 +0900

    add gamepad sample

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + "/client/index.html")
 });
 
+app.get('/gamepadSample', function(req, res) {
+    res.sendFile(__dirname + "/client/gamepadSample/index.html")
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
